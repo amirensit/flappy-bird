27ec65383b30c503585d66f6179955d25c85c6f9
amirensit <amirensit@gmail.com>
2017-04-01 23:27:22 +0200

    flappy bird game

---
+++ flappy bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float speed = 2;
+    public float force = 300;
+
+    void Start()
+    {
+        // Fly towards the right
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+
+    void Update()
+    {
+        // Flap
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Restart
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ flappy bird/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public float speed = 0;
+    public float switchTime = 2;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        InvokeRepeating("Switch", 0, switchTime);
+
+    }
+
+    void Switch()
+    {
+        GetComponent<Rigidbody2D>().velocity *= -1;
+    }
+
+}
